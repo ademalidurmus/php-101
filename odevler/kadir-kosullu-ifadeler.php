e29ad2621f12e6e868cc53105719e8c043d0bf96
Adem Ali Durmuş <ademalidrms@gmail.com>
2020-04-25 22:53:53 +0300

    PHP CS fix

--- odevler/kadir-kosullu-ifadeler.php
+++ odevler/kadir-kosullu-ifadeler.php
@@ -1,19 +1,12 @@
 <?php
-$sayi=5;
+$sayi = 5;
 
-if($sayi==5)
-{
-echo"Sayı 5'e eşittir.\n\n";
-
-}
-else
-{
-  echo"Sayı 5'e eşit değildir.\n";
+if ($sayi == 5) {
+  echo "Sayı 5'e eşittir.\n\n";
+} else {
+  echo "Sayı 5'e eşit değildir.\n";
 }
 
-
-
-for($i=0; $i<=100; $i+=2)
-{
-echo"Sayı{$i}\n";
+for ($i = 0; $i <= 100; $i += 2) {
+  echo "Sayı {$i}\n";
 }
