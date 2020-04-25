fce1b9e5fa319de73bd3a10ec9899f48547e8533
SadikOrtaoglan <ortcellad@gmail.com>
2020-04-25 15:04:18 +0300

    for ile mevsimleri yazdırma

---
+++ alistirmalar/donguler.php
@@ -0,0 +1,13 @@
+<?php
+
+$mevsim= array('ilkbahar', 'yaz', 'sonbahar', 'kış');
+
+$s = count($mevsim); // dizinin eleman sayısı bulunur
+
+for( $i=0; $i<$s; $i++ ){
+
+ 
+    echo $mevsim[$i]."\n";
+}
+
+?>
