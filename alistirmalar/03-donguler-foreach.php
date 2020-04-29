74cd16e0ae147d8fc90f4fed33cca54ecca54c32
Kadir Laçin <kadilacinnn@gmail.com>
2020-04-29 16:18:53 +0300

    Döngüler ve fonksiyolarla ilgili örnekler yaptım.

--- alistirmalar/03-donguler-foreach.php
+++ alistirmalar/03-donguler-foreach.php
@@ -6,7 +6,7 @@ $padisah['Görevi'] = 'Ülke yönetme';
 $padisah['Başarısı'] = 'İstanbul fethi';
 
 foreach ($padisah as $isim => $deger) {
-   echo "{$isim}: {$deger}\n";
+   echo "{$isim}: {$deger} <br>";
 }
 
 $dizi = array("Adi" => "Sadik", "Soyadi" => "ortaoglan", "Site" => "www.forumkktc.com");
@@ -14,3 +14,16 @@ $dizi = array("Adi" => "Sadik", "Soyadi" => "ortaoglan", "Site" => "www.forumkkt
 foreach ($dizi as $key => $value) {
 	echo "$key: $value <br/>";
 }
+
+
+
+/* Kadir'in yaptığı yer.
+$sayilar=array(3, 5, 7, 9, 11);
+$toplam = 0;
+
+foreach($sayilar as $sayi){
+$toplam = $toplam + $sayi;
+}
+
+echo $toplam;
+*/
--- alistirmalar/04-fonksiyonlar-for.php
+++ alistirmalar/04-fonksiyonlar-for.php
@@ -10,4 +10,46 @@ function HelloWorld($sayi) {
 
 HelloWorld(5);
 
-?>
+/* Kadir'in yaptığı yer.
+function toplam()
+{
+	$cevap = 0;
+	for($sayi = 0; $sayi <= 10; $sayi++){
+		
+	$cevap = $cevap + $sayi;
+	
+	}	
+	echo "CEVABINIZ=$cevap";
+}
+toplam();
+
+//Çift sayıyı bulduk
+function deger(){
+	
+	$sayi=2;
+	if($sayi%2==0){
+		return "Sayı Çift'tir.";
+	}
+	else{
+		return "Sayı Tek'tir.";
+		
+	}
+}
+
+echo deger();
+
+//Tek sayıyı bulduk
+function deger(){
+	
+	$sayi=5;
+	if($sayi%2==0){
+		return "Sayı Çift'tir.";
+	}
+	else{
+		return "Sayı Tek'tir.";
+		
+	}
+}
+
+echo deger();
+*/
