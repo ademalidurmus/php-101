afa62b4e9271c2f0f92013f22cb559037e690e25
SadikOrtaoglan <ortcellad@gmail.com>
2020-05-26 22:44:48 +0300

    Taklit kodlarım

--- alistirmalar/08.1-nesneyonelimli-programlama.php
+++ alistirmalar/08.1-nesneyonelimli-programlama.php
@@ -1,28 +1,29 @@
 <?php
 
-class Adam
+class Insan
 {
-    public $adsoyad="Sadık Ortaoğlan";
+    public $ad_soyad = "Sadık Ortaoğlan";
     public $yas;
 
-    public function goster(){
-        printf("%s adlı kişinin yaşı: %s <br>\n",$this->adsoyad,$this->yas);
+    public function goster()
+    {
+        printf("%s adlı kişinin yaşı: %s <br>\n" , $this->adsoyad , $this->yas);
+    }
 }
-  }
 
-class Alan extends Adam
+class Alan extends Insan
 {
-    public $isi="İşsiz";
+    public $isi = "İşsiz";
 }
 
-// $kisi1=new Alan;
-// $kisi1->adsoyad="Kadir Ortaoğlan";
-// $kisi1->yas=27;
-// $kisi1->isi="Python Uzmanı";
+// $kisi1 = new Alan;
+// $kisi1->ad_soyad = "Kadir Ortaoğlan";
+// $kisi1->yas = 27;
+// $kisi1->isi = "Python Uzmanı";
 // printf($kisi1->adsoyad. ' adlı kişi '. $kisi1->yas. 'yaşındadır ve Mesleği'.$kisi1->isi);
 
-// $kisi=new Adam;
-// $kisi->adsoyad="Ahmet aras";
-// $kisi->yas=25;
+// $kisi = new Adam;
+// $kisi->ad_soyad = "Ahmet aras";
+// $kisi->yas = 25;
 // $kisi->goster();
 // print_r($kisi);
